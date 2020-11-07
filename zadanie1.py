b1dd313e435c91b4885c78cf13d6a75ffab91913
Mateusz Kamiński <mathisplay@gmail.com>
2020-11-07 15:43:57 +0100

    Simplified and fixed some code

--- zadanie1.py
+++ zadanie1.py
@@ -21,40 +21,36 @@ with open("trainingData/data.txt", "r") as f:
     splitText = np.zeros((len(text), 2), dtype=object)
     for i in range(len(text)):
         splitText[i] = text[i].rstrip().split(" ")
-finalData = np.zeros((len(text), 7, 5, 3), dtype='float32')
+learningData = np.zeros((len(splitText), 36), dtype='float32')
+correctAnswers = np.zeros(len(splitText), dtype='int')
 print("Analyzing images...")
 for i in range(len(splitText)):
     image = im.open('trainingData/' + splitText[i][0])
-    data = np.array(image.getdata())
-    data = data.reshape((7, 5, 3))
-    finalData[i] = data.copy()
-learningData = np.zeros((len(splitText), 36), dtype='float32')
-correctAnswers = np.zeros(len(splitText), dtype='int')
-for i in range(len(finalData)):
-    for j in range(7):
-        for k in range(5):
-            learningData[i][j * 5 + k] = finalData[i][j][k][0].copy() / 255.0
+    learningData[i] = np.r_[np.array(image.getdata()).flatten()[0::3] / 255.0, 1]
     correctAnswers[i] = int(splitText[i][1])
-    learningData[i][-1] = 1
-
 bestWeights = weights.copy()
 bestLifetime = 0
 learning = 0.001
 lifetime = 0
-while bestLifetime <= 10000:
-    bestDetection = -1
 
+for i in range(10000):
     current = np.random.randint(len(splitText))
     correctAns = correctAnswers[current]
+    noise = np.random.randint(0, 35, (2))
+    learningDataNoise = learningData[current].copy()
+    for a in noise:
+        if learningDataNoise[a] > 0.0:
+            learningDataNoise[a] = 0.0
+        else:
+            learningDataNoise[a] = 1.0
     sum = np.zeros(10, dtype='float32')
     for i in range(10):
-        sum[i] = np.sum(np.multiply(learningData[current], weights[i]))
+        sum[i] = np.sum(np.multiply(learningDataNoise, weights[i]))
     best = np.amax(sum)
     bestId = np.unravel_index(np.argmax(sum, axis=None), sum.shape)[0]
-
     if bestId != correctAns:
-        weights[bestId] = weights[bestId] + (learning * (-2.0) * learningData[current])
-        weights[correctAns] = weights[correctAns] + (learning * (2.0) * learningData[current])
+        weights[bestId] = weights[bestId] + (-1 * learning * learningDataNoise)
+        weights[correctAns] = weights[correctAns] + (learning * learningDataNoise)
         lifetime = 0
     else:
         lifetime += 1
@@ -64,25 +60,19 @@ while bestLifetime <= 10000:
 print("Images analyzed")
 
 image = im.open('trainingData/test.png')
-data = np.array(image.getdata())
-data = data.reshape((7, 5, 3))
-newData = np.zeros((36), dtype='float32')
-for i in range(7):
-    for j in range(5):
-        newData[i * 5 + j] = data[i][j][0].copy() / 255.0
-newData[-1] = 1
+data = np.r_[np.array(image.getdata()).flatten()[0::3] / 255.0, 1]
 testCorrect = np.array([6])
 
 sum = np.zeros(10, dtype='float32')
 print("This drawing could be a:")
 for i in range(10):
-    sum[i] = np.sum(np.multiply(newData, bestWeights[i]))
+    sum[i] = np.sum(np.multiply(data, bestWeights[i]))
 print(np.unravel_index(np.argmax(sum, axis=None), sum.shape)[0])
 
 plt.figure()
 plt.grid(False)
 plt.xticks([])
 plt.yticks([])
-showPic = newData[0:-1]
+showPic = data[0:-1]
 plt.imshow(showPic.reshape(7, 5), cmap=plt.cm.binary)
 plt.show()
